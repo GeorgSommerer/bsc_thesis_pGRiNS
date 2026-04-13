490bffdbf49262bd46604e2e13254a3b9e214c23
GeorgSommerer <gc.sommerer@gmail.com>
2026-04-13 12:21:52 +0200

    Implemented Pipeline to download Dorothea and KEGG datasets and convert them to the proper .topo format

---
+++ Prep_Data/grn_to_topo.R
@@ -0,0 +1,46 @@
+# library(OmnipathR)
+# library(tidyverse)
+# trrust <- import_tf_target_interactions(
+#   resources = "TRRUST",
+#   organism = 9606  
+# ) %>% filter(consensus_direction == TRUE)
+# head(trrust)
+
+
+# Dorothea:
+library(dorothea)
+library(decoupleR)
+library(tidyverse)
+library(OmnipathR)
+library(magrittr)
+load("../Data/Dorothea/dorothea_hs.rda") # Taken from https://github.com/saezlab/dorothea/blob/master/data/dorothea_hs.rda
+dorothea_filtered <- dorothea_hs %>%
+  mutate(mor = as.factor(mor)) %>% 
+  rename(Source=tf,Target=target,Type=mor)
+levels(dorothea_filtered$Type) <- c("2","1")
+
+write_delim(dorothea_filtered %>% filter(confidence %in% c("A","B","C")) %>% select(!confidence),"../Data/Dorothea/dorothea_abc.topo",delim=" ")
+write_delim(dorothea_filtered %>% filter(confidence %in% c("A","B","C","D")) %>% select(!confidence),"../Data/Dorothea/dorothea_abcd.topo",delim=" ")
+write_delim(dorothea_filtered %>% select(!confidence),"../Data/Dorothea/dorothea_abcde.topo",delim=" ")
+
+
+# KEGG:
+
+
+kegg_pw <- kegg_pathways_download(max_expansion = NULL, simplify = TRUE)
+kegg_pw %>% group_by(type) %>% summarise(no_rows = length(type))
+
+print(kegg_pw  %>% group_by(effect) %>% summarise(no_rows = length(effect)), n=100)
+
+# https://www.genome.jp/kegg/xml/docs/
+# https://r.omnipathdb.org/reference/kegg_pathways_download.html
+# https://omnipathdb.org/
+
+# PPrel: PPIs
+# ECrel: enzyme-enzyme successive catalysis (arrow contains numbers, maybe a reaction index?)
+# GErel: gene expression
+# PCrel: protein-compound
+
+
+# Idea: Represent the following effects as activating (1): activation, binding/association, compound, expression
+  # Represent the following effects as inhibiting (2): dissociation, inhibition, repression
